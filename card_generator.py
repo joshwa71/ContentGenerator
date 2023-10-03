68c65f6fd9892eab3523dcbe4b22f3ea16a529e8
Josh O'hara <joshuaohara71@gmail.com>
2023-10-03 16:22:30 +0100

    full pipeline, prompts need tuning

--- card_generator.py
+++ card_generator.py
@@ -104,8 +104,8 @@ if __name__ == "__main__":
     
     parser.add_argument("--categories", nargs="+", default=["climbing"], help="List of categories.")
     parser.add_argument("--num_images", type=int, default=2, help="Number of images to generate for each category.")
-    parser.add_argument("--OAI_api_key", default="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", help="OpenAI API key.")
-    parser.add_argument("--SD_api_key", default="sk-G8km1hjnY1jwHHX8vV9rUap8Jis7iWyra0Pswoezmpx4PaDH", help="Stability API key.")
+    parser.add_argument("--OAI_api_key", default="--", help="OpenAI API key.")
+    parser.add_argument("--SD_api_key", default="--", help="Stability API key.")
     parser.add_argument("--engine_id", default="stable-diffusion-xl-1024-v1-0", help="Engine ID for Stability API.")
 
     args = parser.parse_args()
